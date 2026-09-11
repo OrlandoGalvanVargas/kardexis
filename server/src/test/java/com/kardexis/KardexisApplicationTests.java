fc426517310207de97c4f52a5130df26e9773f62
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-09-10 23:08:24 -0600

    feat: update rename from ApiApplicationTests to KardexisApplicationTests file

--- server/src/test/java/com/kardexis/KardexisApplicationTests.java
+++ server/src/test/java/com/kardexis/KardexisApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kardexis.api;
+package com.kardexis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class KardexisApplicationTests {
 
 	@Test
 	void contextLoads() {
