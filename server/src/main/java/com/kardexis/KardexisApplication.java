fc9dcf7fe5904173b0eed9677d277854751f1e92
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-09-10 23:01:37 -0600

    feat: udpate name from ApiApplication to KardexisApplication

--- server/src/main/java/com/kardexis/KardexisApplication.java
+++ server/src/main/java/com/kardexis/KardexisApplication.java
@@ -1,13 +1,13 @@
-package com.kardexis.api;
+package com.kardexis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class KardexisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(KardexisApplication.class, args);
 	}
 
 }
