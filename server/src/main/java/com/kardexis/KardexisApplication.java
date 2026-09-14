bd21cd32ecc7c0a3c0c4ad96b896951d5e865f60
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-09-13 21:45:40 -0600

    feat: udpate KardexisApplication

--- server/src/main/java/com/kardexis/KardexisApplication.java
+++ server/src/main/java/com/kardexis/KardexisApplication.java
@@ -2,12 +2,17 @@ package com.kardexis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
+@ConfigurationPropertiesScan
 public class KardexisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KardexisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KardexisApplication.class, args);
+    }
 
 }
